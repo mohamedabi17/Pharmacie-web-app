49295a21a39d82842d7af72ab3a9176b9d792c86
mohamedabi17 <mohamedabi1485@gmail.com>
2024-02-27 00:00:42 +0100

    'done'

--- Pharmacie_front/app/sellButton.tsx
+++ Pharmacie_front/app/sellButton.tsx
@@ -9,7 +9,7 @@ const SellButton = () => {
         <div className="hero-content text-center text-neutral-content">
             <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Search You medicamanet Know</h1>
-            <p className="mb-5">The best  expierience you can get.</p>
+            <p className="mb-5">The best  expierience you can get. Be Healthy</p>
             <button className="btn glass"><Link href="/search">Search</Link></button>
             </div>
         </div>
